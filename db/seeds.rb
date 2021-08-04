8710b382418165044670836c8cea6022fcdb0ce2
Romain Losey <romain.losey@gmail.com>
2021-08-04 11:47:36 +0200

    Finished exercise

--- db/seeds.rb
+++ db/seeds.rb
@@ -5,3 +5,17 @@
 #
 #   movies = Movie.create([{ name: 'Star Wars' }, { name: 'Lord of the Rings' }])
 #   Character.create(name: 'Luke', movie: movies.first)
+
+require 'faker'
+
+puts 'Creating 10 fake restaurants...'
+
+10.times do
+  restaurant = Restaurant.new(
+    name:    "#{Faker::Name.name}'s restaurant",
+    address:  Faker::Address.full_address,
+    category: ["chinese", "italian", "japanese", "french", "belgian"].sample
+  )
+  restaurant.save
+end
+puts 'Finished!'
